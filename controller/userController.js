97722ef26775ec5c2632c05bb5d70ee2b75e958a
Mussued <skossm@outlook.com>
2023-03-29 17:05:46 +0800

    文件上传操作

--- controller/userController.js
+++ controller/userController.js
@@ -1,5 +1,8 @@
+const fs = require("fs");
+const { promisify } = require("util");
 const { User } = require("../model/index");
 const { createToken } = require("../util/jwt");
+const rename = promisify(fs.rename);
 
 // 用户注册
 exports.register = async (req, res) => {
@@ -29,6 +32,22 @@ exports.update = async (req, res) => {
   var id = req.user.userinfo._id;
   var dbBack = await User.findByIdAndUpdate(id, req.body, { new: true });
   res.status(202).json({ user: dbBack });
+};
+
+// 用户头像上传
+exports.headimg = async (req, res) => {
+  var fileArr = req.file.originalname.split(".");
+  var filetype = fileArr[fileArr.length - 1];
+
+  try {
+    await rename(
+      "./public/" + req.file.filename,
+      "./public/" + req.file.filename + "." + filetype
+    );
+    res.status(201).json({ filepath: req.file.filename + "." + filetype });
+  } catch (error) {
+    res.status(500).json({ err: error });
+  }
 };
 
 exports.list = async (req, res) => {
--- router/user.js
+++ router/user.js
@@ -3,12 +3,20 @@ const router = express.Router();
 const userController = require("../controller/userController");
 const validator = require("../middleware/validator/userValidator");
 const { verifyToken } = require("../util/jwt");
+const multer = require("multer");
+const upload = multer({ dest: "public/" });
 
 router
   .post("/registers", validator.register, userController.register)
   .post("/logins", validator.login, userController.login)
   .get("/lists", verifyToken, userController.list)
   .put("/", verifyToken, validator.update, userController.update)
+  .post(
+    "/headimg",
+    verifyToken,
+    upload.single("headimg"),
+    userController.headimg
+  )
   .delete("/", userController.delete);
 
 module.exports = router;
