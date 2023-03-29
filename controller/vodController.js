e9e02178fe1c4133c950410eec51cc3ec65ada5b
Mussued <skossm@outlook.com>
2023-03-29 19:29:54 +0800

    获取VoD上传凭证及地址

---
+++ controller/vodController.js
@@ -0,0 +1,28 @@
+var RPCClient = require("@alicloud/pop-core").RPCClient;
+
+function initVodClient(accessKeyId, accessKeySecret) {
+  var regionId = "cn-shanghai"; // 点播服务接入地域
+  var client = new RPCClient({
+    //填入AccessKey信息
+    accessKeyId: accessKeyId,
+    accessKeySecret: accessKeySecret,
+    endpoint: "http://vod." + regionId + ".aliyuncs.com",
+    apiVersion: "2017-03-21",
+  });
+
+  return client;
+}
+
+exports.getvod = async (req, res) => {
+  var client = initVodClient("<Your AccessKeyId>", "<Your AccessKeySecret>");
+
+  const vodback = await client.request(
+    "CreateUploadVideo",
+    {
+      Title: "this is a sample",
+      FileName: "filename.mp4",
+    },
+    {}
+  );
+  res.status(200).json({ vod: vodback });
+};
--- router/video.js
+++ router/video.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const router = express.Router();
 const videoController = require("../controller/videoController");
+const vodController = require("../controller/vodController");
 
 router.get("/lists", videoController.list);
+router.get("/getvod", vodController.getvod);
 
 module.exports = router;
